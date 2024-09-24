b338d8adc1e7250d80ae902d739b92f34acb1c86
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-24 19:37:22 +0300

    Update App.js

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,27 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { NativeRouter } from 'react-router-native';
 
 import Main from './src/components/Main';
 
-const App = () => {
+const App = () => {
    return (
     <>
         <NativeRouter>
             <Main />
         </NativeRouter>
         <StatusBar style="auto"/>
-        </>
-    )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </>
+    );
+};
 
 export default App;
