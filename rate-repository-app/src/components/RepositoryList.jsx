bd79a830bf58073ad40bd961842482716f8a2042
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-24 17:06:56 +0300

    Finish exercise 10.5

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,12 +1,42 @@
-import { FlatList, View, StyleSheet, Text } from 'react-native';
+import { FlatList, Image, View, StyleSheet } from 'react-native';
+import Text from './Text';
+
+const formatter = (n) => {
+    return Math.abs(n) > 999 ? (Math.abs(n) / 1000).toFixed(1) + "k" : n
+};
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
+    backgroundColor: "whitesmoke"
   },
   view : {
     padding: 20,
-    
+  },
+  horizontalView: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  langText: {
+    backgroundColor: "lightblue",
+    padding: 4,
+    borderRadius: 5,
+    overflow: "hidden",
+    alignSelf: "flex-start",
+    marginTop: 5
+  },
+  stats: {
+    alignItems: "center",
+  },
+  imgDesc: {
+    flexDirection: "row",
+    marginBottom: 20
+  },
+  repoImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+    marginRight: 12
   }
 });
 
@@ -66,12 +96,32 @@ const RepositoryList = () => {
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) =>
         <View style={styles.view}>
-            <Text>Name: { item.fullName }</Text>
-            <Text>Desc: { item.description }</Text>
-            <Text>Language: { item.language }</Text>
-            <Text>Forked: { item.forksCount }</Text>
-            <Text>Reviews: { item.reviewCount }</Text>
-            <Text>Rating: { item.ratingAverage }</Text>
+            <View style={styles.imgDesc}>
+                <Image source={{uri: item.ownerAvatarUrl}} style={styles.repoImg}/>
+                <View>
+                    <Text fontWeight={"bold"}>{ item.fullName }</Text>
+                    <Text style={{ marginTop: 5 }}>{ item.description }</Text>
+                    <Text style={styles.langText}>{ item.language }</Text>
+                </View>
+            </View>
+            <View style={styles.horizontalView}>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.stargazersCount) }</Text>
+                    <Text>Stars</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.forksCount) }</Text>
+                    <Text>Forks</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.reviewCount) }</Text>
+                    <Text>Reviews</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ item.ratingAverage }</Text>
+                    <Text>Rating</Text>
+                </View>
+            </View>
         </View>
     }
     />
