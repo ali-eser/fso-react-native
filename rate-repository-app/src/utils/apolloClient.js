6a34a6fc76ac52c7d4195b43d71f4c00d8906a48
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-30 20:32:33 +0300

    Update apolloClient.js

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
     return new ApolloClient({
-        uri: 'http://192.168.1.24:4000/graphql',
+        uri: 'http://192.168.1.23:4000/graphql',
         cache: new InMemoryCache()
     });
 };
