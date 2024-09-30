333190e74c76def62219439918d7cc506a2c1c2b
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-30 21:25:50 +0300

    Finish exercise 10.11

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { loading, error, data } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network"
+  });
 
   const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch('http://192.168.1.23:5001/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
+    if (error) {
+			console.log(error);
+		}
+    setRepositories(data.repositories);
   };
 
   useEffect(() => {
