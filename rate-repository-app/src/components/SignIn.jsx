6742322bcee50d40b766253d0c73d8fa2cd9b8d7
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-10-04 02:54:39 +0300

    Update SignIn.jsx

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -6,33 +6,33 @@ import * as yup from "yup";
 import useSignIn from "../hooks/useSignIn";
 
 const validationSchema = yup.object().shape({
-    username: yup
-        .string()
-        .required("You must set a username"),
-    password: yup
-        .string()
-        .required("You must set a password")
+  username: yup
+    .string()
+    .required("You must set a username"),
+  password: yup
+    .string()
+    .required("You must set a password")
 });
 
 const styles = StyleSheet.create({
-    textInput: {
-        borderWidth: 2,
-        borderRadius: 5,
-        borderColor: "grey",
-        height: 50,
-        margin: 10,
-        padding: 10
-    },
-    textInputError: {
-        borderColor: "#d73a4a"
-    },
-    pressable: {
-        backgroundColor: "lightblue",
-        borderRadius: 5,
-        alignSelf: "center",
-        width: "auto",
-        padding: 10
-    }
+  textInput: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "grey",
+    height: 50,
+    margin: 10,
+    adding: 10
+  },
+  textInputError: {
+    borderColor: "#d73a4a"
+  },
+  pressable: {
+    backgroundColor: "lightblue",
+    borderRadius: 5,
+    alignSelf: "center",
+    width: "auto",
+    padding: 10
+  }
 });
 
 
@@ -61,35 +61,35 @@ const SignIn = () => {
       onSubmit
   });
 
-    return (
-        <View style={styles.view}>
-            <TextInput
-                style={[styles.textInput, formik.errors.username ? styles.textInputError : styles.textInput]}
-                placeholder="username"
-                value={formik.values.username}
-                onChangeText={formik.handleChange("username")}
-            />
-            {formik.touched.username && formik.errors.username && (
-                <Text style={{ color: "#d73a4a", marginLeft: 10 }}>{formik.errors.username}</Text>
-            )}
-            <TextInput
-                style={[styles.textInput, formik.errors.password ? styles.textInputError : styles.textInput]}
-                secureTextEntry
-                placeholder="password"
-                value={formik.values.password}
-                onChangeText={formik.handleChange("password")}
-            />
-            {formik.touched.password && formik.errors.password && (
-                <Text style={{ color: "#d73a4a", marginLeft: 10 }}>{formik.errors.password}</Text>
-            )}
-            <Pressable 
-                onPress={formik.handleSubmit}
-                style={styles.pressable}
-            >
-                <Text>Sign in</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.view}>
+      <TextInput
+        style={[styles.textInput, formik.errors.username ? styles.textInputError : styles.textInput]}
+        placeholder="username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange("username")}
+      />
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{ color: "#d73a4a", marginLeft: 10 }}>{formik.errors.username}</Text>
+      )}
+      <TextInput
+        style={[styles.textInput, formik.errors.password ? styles.textInputError : styles.textInput]}
+        secureTextEntry
+        placeholder="password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange("password")}
+      />
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: "#d73a4a", marginLeft: 10 }}>{formik.errors.password}</Text>
+      )}
+      <Pressable
+        onPress={formik.handleSubmit}
+        style={styles.pressable}
+      >
+      <Text>Sign in</Text>
+        </Pressable>
+      </View>
+  );
 };
 
 export default SignIn;
