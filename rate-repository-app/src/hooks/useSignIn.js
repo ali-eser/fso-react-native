9b880a7599fed0de65772ca6944227ab47f1c245
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-10-09 01:19:21 +0300

    Finish exercise 10.15

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,16 +1,24 @@
 import { useMutation } from "@apollo/client";
 import { AUTHENTICATION } from "../graphql/mutations";
 
+import { useApolloClient } from "@apollo/client";
+import useAuthStorage from "./useAuthStorage";
+
 const useSignIn = () => {
+  const apolloClient = useApolloClient();
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTHENTICATION);
 
   const signIn = async ({ username, password }) => {
-    mutate({
+    const {data} = await mutate({
       variables: {
         username: username,
         password: password
       }
     });
+
+    await authStorage.setAccessToken(data.authenticate.accessToken);
+    apolloClient.resetStore();
   };
 
 
