ebf271c16a1286a773d48b641875a041f74a5987
Ali Eser <115903331+ali-eser@users.noreply.github.com>
2024-10-10 18:53:11 +0300

    Update useMe.js

--- rate-repository-app/src/hooks/useMe.js
+++ rate-repository-app/src/hooks/useMe.js
@@ -2,24 +2,15 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
 import { GET_ME } from "../graphql/queries";
-import useAuthStorage from "./useAuthStorage";
 
 const useMe = () => {
-  const authStorage = useAuthStorage();
   const [meData, setMeData] = useState(null);
-  const [currentAccessToken, setCurrentAccessToken] = useState(null);
-
-  const fetchToken = async () => {
-    const accessToken = await authStorage.getAccessToken();
-    setCurrentAccessToken(accessToken);
-  };
 
   const { error, data } = useQuery(GET_ME, {
     fetchPolicy: "network-only"
   });
 
   useEffect(() => {
-    fetchToken();
     if (data) {
       setMeData(data.me);
     }
@@ -27,7 +18,7 @@ const useMe = () => {
     if (error) {
       console.log(error);
     }
-  }, [data, error, currentAccessToken]);
+  }, [data, error]);
 
   return [meData];
 };
