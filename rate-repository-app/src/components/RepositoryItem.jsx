62ce4e41836e1182b935fd1ec1eecfa8a0c469e4
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-10-24 23:14:14 +0300

    Update RepositoryItem.jsx

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,36 @@
+import { Image, View, StyleSheet } from 'react-native';
+import Text from './Text';
+
+const RepositoryItem = () => {
+
+  return (
+    <View style={styles.view}>
+            <View style={styles.imgDesc}>
+                <Image source={{uri: item.ownerAvatarUrl}} style={styles.repoImg}/>
+                <View>
+                    <Text fontWeight={"bold"}>{ item.fullName }</Text>
+                    <Text style={{ marginTop: 5 }}>{ item.description }</Text>
+                    <Text style={styles.langText}>{ item.language }</Text>
+                </View>
+            </View>
+            <View style={styles.horizontalView}>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.stargazersCount) }</Text>
+                    <Text>Stars</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.forksCount) }</Text>
+                    <Text>Forks</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ formatter(item.reviewCount) }</Text>
+                    <Text>Reviews</Text>
+                </View>
+                <View style={styles.stats}>
+                    <Text fontWeight={"bold"}>{ item.ratingAverage }</Text>
+                    <Text>Rating</Text>
+                </View>
+            </View>
+        </View>
+  )
+}
