11f5969f9f169f1cd09aa9579dc36df3058b69f2
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-30 23:23:16 +0300

    Update queries.js

--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -32,6 +32,20 @@ export const GET_ONE = gql`
       reviewCount
       stargazersCount
       url
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
 `
