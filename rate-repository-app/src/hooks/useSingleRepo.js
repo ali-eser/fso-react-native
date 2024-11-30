dab10d9d12bd604b8cd89bfa82d357a5360b5bd8
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-30 19:15:32 +0300

    Create useSingleRepo.js

---
+++ rate-repository-app/src/hooks/useSingleRepo.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import {GET_ONE} from '../graphql/queries'
+
+const useSingleRepo = (id) => {
+    const [singleRepo, setSingleRepo] = useState();
+
+    const { loading, error, data } = useQuery(GET_ONE, {
+        fetchPolicy: "cache-and-network",
+        variables: { id }
+    });
+
+    try {
+        if (!error && !loading && data) {
+            return data;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default useSingleRepo;
