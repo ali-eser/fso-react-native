aa10bcf66e8e012b09d5973b50f0d79cf520b6b5
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-11-30 19:23:45 +0300

    Update RepositoryList.jsx

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -56,31 +56,31 @@ const RepositoryList = () => {
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) =>
         <View style={styles.view}>
-            <View style={styles.imgDesc}>
-                <Image source={{uri: item.ownerAvatarUrl}} style={styles.repoImg}/>
-                <View>
-                    <Text fontWeight={"bold"}>{ item.fullName }</Text>
-                    <Text style={{ marginTop: 5 }}>{ item.description }</Text>
-                    <Text style={styles.langText}>{ item.language }</Text>
-                </View>
+          <View style={styles.imgDesc}>
+            <Image source={{uri: item.ownerAvatarUrl}} style={styles.repoImg}/>
+            <View>
+              <Text fontWeight={"bold"}>{ item.fullName }</Text>
+              <Text style={{ marginTop: 5 }}>{ item.description }</Text>
+              <Text style={styles.langText}>{ item.language }</Text>
+            </View>
             </View>
             <View style={styles.horizontalView}>
-                <View style={styles.stats}>
-                    <Text fontWeight={"bold"}>{ formatter(item.stargazersCount) }</Text>
-                    <Text>Stars</Text>
-                </View>
-                <View style={styles.stats}>
-                    <Text fontWeight={"bold"}>{ formatter(item.forksCount) }</Text>
-                    <Text>Forks</Text>
-                </View>
-                <View style={styles.stats}>
-                    <Text fontWeight={"bold"}>{ formatter(item.reviewCount) }</Text>
-                    <Text>Reviews</Text>
-                </View>
-                <View style={styles.stats}>
-                    <Text fontWeight={"bold"}>{ item.ratingAverage }</Text>
-                    <Text>Rating</Text>
-                </View>
+              <View style={styles.stats}>
+                <Text fontWeight={"bold"}>{ formatter(item.stargazersCount) }</Text>
+                <Text>Stars</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text fontWeight={"bold"}>{ formatter(item.forksCount) }</Text>
+                <Text>Forks</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text fontWeight={"bold"}>{ formatter(item.reviewCount) }</Text>
+                <Text>Reviews</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text fontWeight={"bold"}>{ item.ratingAverage }</Text>
+                <Text>Rating</Text>
+              </View>
             </View>
         </View>
     }
