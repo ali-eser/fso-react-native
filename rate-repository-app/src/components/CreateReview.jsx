03413a1957ae9d45daf5b09f079e67c514d8a584
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-01 18:18:52 +0300

    Update CreateReview.jsx

--- rate-repository-app/src/components/CreateReview.jsx
+++ rate-repository-app/src/components/CreateReview.jsx
@@ -4,19 +4,72 @@ import { Pressable, StyleSheet, TextInput, View  } from "react-native";
 import Text from "./Text";
 import * as yup from "yup";
 
+const validationSchema = yup.object().shape({
+  repository: yup
+    .string()
+    .required("You must specify the name of the repository"),
+  username: yup
+    .string()
+    .required("You must specify the owner of the repository"),
+  rating: yup
+    .number()
+    .required("You must enter an integer between 0 and 100"),
+  review: yup
+    .string()
+    .optional()
+});
+
+const initialValues = {
+  repository: "",
+  username: "",
+  rating: 0,
+  review: ""
+};
+
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "grey",
+    height: 50,
+    margin: 10,
+    padding: 10
+  },
+  textInputError: {
+    borderColor: "#d73a4a"
+  },
+  pressable: {
+    backgroundColor: "lightblue",
+    borderRadius: 5,
+    alignSelf: "center",
+    width: "auto",
+    padding: 10
+  }
+});
+
 const CreateReview = () => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema
+  });
+
   return (
-    <View>
+    <View style={styles.view}>
       <TextInput
+        style={[styles.textInput, formik.errors.repository ? styles.textInputError : styles.textInput]}
         placeholder={"Repository owner name"}
       />
       <TextInput
+        style={[styles.textInput, formik.errors.username ? styles.textInputError : styles.textInput]}
         placeholder={"Repository name"}
       />
       <TextInput
+        style={[styles.textInput, formik.errors.rating ? styles.textInputError : styles.textInput]}
         placeholder={"Rating between 0 and 100"}
       />
       <TextInput
+        style={styles.textInput}
         placeholder={"Review"}
       />
     </View>
