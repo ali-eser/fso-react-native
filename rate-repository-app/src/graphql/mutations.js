e5aeaae5d0696a389bf377650d29178ec5b176b3
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-02 20:43:51 +0300

    Update mutations.js

--- rate-repository-app/src/graphql/mutations.js
+++ rate-repository-app/src/graphql/mutations.js
@@ -7,3 +7,16 @@ export const AUTHENTICATION = gql`
     }
   }
 `;
+
+export const CREATE_REVIEW = gql`
+  mutation CreateReview($ownerName: String!, $repositoryName: String!, $rating: Int!, $text: String) {
+    createReview(review: {
+      ownerName: $ownerName,
+      repositoryName: $repositoryName,
+      rating: $rating,
+      text: $text
+    }) {
+      repositoryId
+    }
+  }
+`;
