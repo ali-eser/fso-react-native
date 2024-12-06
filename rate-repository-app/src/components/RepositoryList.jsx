5930830d01ed493365bf1450bd0f6ab225e7c944
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-06 23:58:54 +0300

    Finish exercise 10.24

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,9 +1,9 @@
-import {FlatList, Image, View, StyleSheet, Pressable} from 'react-native';
+import {FlatList, Image, View, StyleSheet, Pressable, TextInput} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { Link } from 'react-router-native';
 import Text from './Text';
 import useRepositories from '../hooks/useRepositories';
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 
 const formatter = (n) => {
     return Math.abs(n) > 999 ? (Math.abs(n) / 1000).toFixed(1) + "k" : n
@@ -13,6 +13,14 @@ const styles = StyleSheet.create({
   separator: {
     height: 10,
     backgroundColor: "whitesmoke"
+  },
+  textInput: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "grey",
+    height: 50,
+    margin: 10,
+    padding: 10
   },
   view : {
     padding: 20,
@@ -46,40 +54,83 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler); // Cleanup
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
 const RepositoryList = () => {
   const [order, setOrder] = useState("CREATED_AT");
   const [direction, setDirection] = useState("DESC");
+  const [keyword, setKeyword] = useState("");
+  const debouncedKeyword = useDebounce(keyword, 300);
 
-  const { repos } = useRepositories(order, direction);
+  const { repos, loading, error, refetch } = useRepositories(order, direction, debouncedKeyword);
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus(); // Re-focus TextInput after re-render
+    }
+  });
 
   const repositoryNodes = repos
     ? repos.edges.map(edge => edge.node)
     : [];
 
+  const onSearch = async (value) => {
+    setKeyword(value);
+  }
+
+  if (error) return <Text>Error: {error}</Text>;
+
   return (
     <>
-      <Picker
-        selectedValue={order}
-        onValueChange={(itemValue, itemIndex) => {
-          if (itemValue === "latest") {
-            setOrder("CREATED_AT");
-            setDirection("DESC");
-          } else if (itemValue === "highest") {
-            setOrder("RATING_AVERAGE");
-            setDirection("DESC");
-          } else if (itemValue === "lowest") {
-            setOrder("RATING_AVERAGE");
-            setDirection("ASC");
-          }
-        }
-        }>
-        <Picker.Item label="Latest repositories" value="latest" />
-        <Picker.Item label="Highest rated repositories" value="highest" />
-        <Picker.Item label="Lowest rated repositories" value="lowest" />
-      </Picker>
       <FlatList
         data={repositoryNodes}
         ItemSeparatorComponent={ItemSeparator}
+        ListHeaderComponent={
+        <>
+          <TextInput
+            ref={inputRef}
+            value={keyword}
+            placeholder="Search"
+            onChangeText={onSearch}
+            style={{ padding: 8, borderColor: "gray", borderWidth: 1 }}
+          />
+          <Picker
+            selectedValue={order}
+            onValueChange={(itemValue, itemIndex) => {
+              if (itemValue === "latest") {
+                setOrder("CREATED_AT");
+                setDirection("DESC");
+              } else if (itemValue === "highest") {
+                setOrder("RATING_AVERAGE");
+                setDirection("DESC");
+              } else if (itemValue === "lowest") {
+                setOrder("RATING_AVERAGE");
+                setDirection("ASC");
+              }
+            }
+            }>
+            <Picker.Item label="Latest repositories" value="latest" />
+            <Picker.Item label="Highest rated repositories" value="highest" />
+            <Picker.Item label="Lowest rated repositories" value="lowest" />
+          </Picker>
+        </>
+        }
+        ListEmptyComponent={() => <Text>No repositories found</Text>}
         renderItem={({ item }) =>
           <Pressable>
             <Link to={`repo/${item.id}`}>
