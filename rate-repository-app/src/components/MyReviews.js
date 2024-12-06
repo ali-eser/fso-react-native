d9cce8ccbce606a0d7d3a895a51aa67aee611e3e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-12-07 01:34:53 +0300

    Update MyReviews.js

--- rate-repository-app/src/components/MyReviews.js
+++ rate-repository-app/src/components/MyReviews.js
@@ -1,17 +1,27 @@
-import { FlatList, Image, View, Button, StyleSheet } from 'react-native';
+import {FlatList, Image, View, Button, StyleSheet, Pressable} from 'react-native';
 import useMe from "../hooks/useMe";
 import Text from './Text';
 import {useEffect} from "react";
+import {useNavigate} from "react-router-native";
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
     backgroundColor: "whitesmoke"
+  },
+  view : {
+    padding: 20,
+  },
+  buttonView: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   }
 });
 
 const  MyReviews = () => {
   const { meData, error, refetch, loading } = useMe(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     refetch();
@@ -21,6 +31,7 @@ const  MyReviews = () => {
     ? meData.reviews.edges.map(edge => edge.node)
     : [];
 
+  console.log(reviewNodes.repository);
   const ItemSeparator = () => <View style={styles.separator} />;
 
   if (loading) return <Text>Loading...</Text>;
@@ -32,10 +43,23 @@ const  MyReviews = () => {
         data={reviewNodes}
         ItemSeparatorComponent={ItemSeparator}
         renderItem={({ item } ) =>
-          <View>
+          <View style={styles.view}>
+            <Text fontWeight={"bold"}>{`${item.repository.id}`}</Text>
             <Text fontWeight={"bold"}>{`${item.rating}`}</Text>
             <Text>{item.createdAt.slice(0,10)}</Text>
             <Text>{ item.text }</Text>
+            <View style={styles.buttonView}>
+              <Button
+                title={"View repository"}
+                color={"blue"}
+                onPress={() => navigate(`/repo/${item.repository.id}`)}
+              />
+              <Button
+                title={"Delete repository"}
+                color={"red"}
+                onPress={() => navigate(`/repo/${item.repository.id}`)}
+              />
+            </View>
           </View>
         }/>
     </View>
